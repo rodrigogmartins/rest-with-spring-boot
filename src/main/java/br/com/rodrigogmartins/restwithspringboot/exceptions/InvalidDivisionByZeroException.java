36518a65de20eb5cd45b7909600745ee5c506a63
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2021-03-27 20:08:29 -0300

    Adding Person model

--- src/main/java/br/com/rodrigogmartins/restwithspringboot/exceptions/InvalidDivisionByZeroException.java
+++
@@ -1,9 +0,0 @@
-package br.com.rodrigogmartins.restwithspringboot.exceptions;
-
-public class InvalidDivisionByZeroException extends RuntimeException {
-
-    public InvalidDivisionByZeroException(String exception) {
-        super(exception);
-    }
-
-}
---
+++ src/main/java/br/com/rodrigogmartins/restwithspringboot/models/Person.java
@@ -0,0 +1,74 @@
+package br.com.rodrigogmartins.restwithspringboot.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private byte age;
+
+    public Person(String id, String firstName, String lastName, String email, byte age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && id.equals(person.id) && firstName.equals(person.firstName) && lastName.equals(person.lastName) && email.equals(person.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, age);
+    }
+}
